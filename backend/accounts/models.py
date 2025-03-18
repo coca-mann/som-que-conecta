05b080f608db9355819df4b4d6745e3ed42241b9
Coca-Mann <juliano.ostroski@gmail.com>
2025-03-17 23:05:31 -0400

    Create models on app 'accounts'; Adjust settings to add root for media; Create folder 'media' on root folder; Update gitignore; Update 'requirements.txt'

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+GENDER_CHOICES = [
+    ('M', 'Masculino'),
+    ('F', 'Feminino'),
+    ('O', 'Outros'),
+]
+
+AUTH_PROVIDER = [
+    ('local', 'Autenticação local'),
+    ('google', 'Autenticação SSO Google'),
+]
+
+class UserType(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.ForeignKey(UserType, on_delete=models.PROTECT, null=False, blank=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg', blank=True)
+    auth_provider = models.CharField(choices=AUTH_PROVIDER, blank=True, null=True)
+    sso_id = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    # modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user
