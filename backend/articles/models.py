7c50d000c17ddb389663d42cfa0f82635f036246
Coca-Mann <juliano.ostroski@gmail.com>
2025-03-18 22:05:32 -0400

    Create models for 'articles' app

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Nome da Tag')
+
+class Article(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False, verbose_name='Título')
+    content = models.JSONField(verbose_name='Conteúdo do Artigo')
+    author = models.CharField(max_length=255, null=True, blank=True, verbose_name='Autor')
+    tags = models.ManyToManyField(Tag, related_name='articles', blank=True, verbose_name='Tags')
+    is_published = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+class ArticleComments(models.Model):
+    article_id = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    content = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class ArticleFavorites(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    article_id = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
