ed5d2508970074c3750ed55685d91d1c3f3ce483
Coca-Mann <juliano.ostroski@gmail.com>
2025-03-20 20:40:23 -0400

    Update admin for 'notifications' app

--- backend/notifications/admin.py
+++ backend/notifications/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Notifications
 
-# Register your models here.
+@admin.register(Notifications)
+class NotificationsAdmin(admin.ModelAdmin):
+    list_display = ('user_id__username', 'message')
