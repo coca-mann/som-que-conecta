0e6f27a8a283ebed08d44d9b71f3697c6e89ce2f
Coca-Mann <juliano.ostroski@gmail.com>
2025-03-20 20:18:55 -0400

    Create admin configuration for 'lessons' app

--- backend/lessons/admin.py
+++ backend/lessons/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
+from django import forms
+from django_ckeditor_5.widgets import CKEditor5Widget
+from .models import Lessons, LearningPaths, UserLearningProgress
 
+admin.site.register(LearningPaths)
+admin.site.register(UserLearningProgress)
+
+@admin.register(Lessons)
+class LessonsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'order')
