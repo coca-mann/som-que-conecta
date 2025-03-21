8a90151182dccfae9a3efee4959ac4b1f280520f
Coca-Mann <juliano.ostroski@gmail.com>
2025-03-20 21:13:49 -0400

    Configure settings.py to work with DRF SimpleJWT; Add new urls for api tokens and api token refresh

--- backend/settings.py
+++ backend/settings.py
@@ -237,3 +237,9 @@ CKEDITOR_5_CONFIGS = {
 
 # Define a constant in settings.py to specify file upload permissions
 CKEDITOR_5_FILE_UPLOAD_PERMISSION = "staff"  # Possible values: "staff", "authenticated", "any"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
