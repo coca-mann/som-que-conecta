e9c5bfb3ed39cb2a28356c516e1bfb3a7a497a64
Coca-Mann <juliano.ostroski@gmail.com>
2025-03-20 21:03:39 -0400

    Update urls paths to add 'accounts' app urls

--- backend/urls.py
+++ backend/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
+    path('api/accounts/', include('backend.accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
