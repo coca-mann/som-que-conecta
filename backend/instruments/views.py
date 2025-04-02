e2961ed09564eae29800713cb7af0cc81cc584a4
Coca-Mann <juliano.ostroski@gmail.com>
2025-04-01 21:36:21 -0400

    Create views for app 'instruments'

--- backend/instruments/views.py
+++ backend/instruments/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .models import (
+    InstrumentTypes,
+    InstrumentBrands,
+    InstrumentAvailability,
+    InstrumentBookings,
+    UserInstrument
+)
 
-# Create your views here.
+
+class InstrumentTypeViewSet(viewsets.ModelViewSet):
+    queryset = InstrumentTypes.objects.all()
+    serializer_class = InstrumentTypeSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class InstrumentBrandsViewSet(viewsets.ModelViewSet):
+    queryset = InstrumentBrands.objects.all()
+    serializer_class = InstrumentBrandsSerializer
+
+
+class InstrumentAvailabilityViewSet(viewsets.ModelViewSet):
+    queryset = InstrumentAvailability.objects.all()
+    serializer_class = InstrumentAvailabilitySerializer
+
+
+class InstrumentBookingViewSet(viewsets.ModelViewSet):
+    queryset = InstrumentBookings.objects.all()
+    serializer_class = InstrumentBookingsSerializer
+
+
+class UserInstrumentViewSet(viewsets.ModelViewSet):
+    serializer_class = UserInstrumentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return UserInstrument.objects.filter(user_id=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user)
